43b2f73da3d6ab51a7cb28fdf48dffe2fdc6da6a
NBZion <subtopewds0921@gmail.com>
2025-12-17 12:02:37 +0800

    init

---
+++ __main__.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+import os
+import dotenv
+
+## Load DotENV
+dotenv.load_dotenv()
+
+intents=discord.Intents.default()
+intents.message_content = True
+intents.guilds = True
+intents.members = True
+bot = commands.Bot(command_prefix="nb!",intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is ready!")
+
+
+if __name__ == "__main__":
+    for cog in os.listdir("./cogs"):
+        if cog != "__pycache__":
+            bot.load_extension(f'cogs.{cog[:-3]}')
+            print("Loaded " + cog)
+    
+    bot.run(os.getenv("TOKEN"))
---
+++ cogs/music.py
@@ -0,0 +1,43 @@
+import discord
+import os
+from discord.ext import commands
+from discord.commands import Option
+import requests
+from requests.auth import HTTPBasicAuth
+import dotenv
+
+
+dotenv.load_dotenv()
+
+## Temp Variable for Whitelisted Servers
+endpoint = "https://dab.yeet.su/api"
+whitelistedServers = ["1053879369011044444"]
+
+class music(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.slash_command(guild_ids=whitelistedServers, description="Searches DAB Servers For The Song You Want")
+    async def search_song(self, ctx, songname: str):
+        # Initialize Session
+        session = requests.Session()
+        # Login
+        payload = {"email": os.getenv("EMAIL"), "password": os.getenv("PASSWORD")}
+        loginResponse = session.post(endpoint+"/auth/login",json=payload)
+
+        if loginResponse.status_code != 200:
+            return
+        
+
+
+        # Search File
+        searchResponse = session.get(endpoint + f"/search?q={songname}&offset=0&type=track")
+        if searchResponse.status_code == 200:
+            await ctx.respond(searchResponse.content)
+        else:
+            await ctx.respond(searchResponse.status_code)
+        
+
+
+def setup(bot):
+    bot.add_cog(music(bot))
